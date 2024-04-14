0bc10a2fd90a47cd0c777df1db8dc831e9d62520
dgrimaliuc <116957079+dgrimaliuc@users.noreply.github.com>
2024-04-15 00:16:46 +0300

    Added some features and validations (#2)
    Co-authored-by: denis.grimaliuc <denis.grimaliuc@crunchyroll.com>

--- enable-cors.js
+++ enable-cors.js
@@ -1,10 +1,8 @@
 const express = require('express');
 
-process.env.CORSANYWHERE_WHITELIST = 'https://localhost';
-
 var app = express();
 var cors = require('cors');
-const { isValidHostName, withCORS } = require('./lib/utils');
+const { isValidHostName, withCORS, getHandler } = require('./lib/utils');
 app.use(express.json());
 
 app.use(
@@ -16,38 +14,8 @@ app.use(
   })
 );
 
-app.post('/**', async function (req, res) {
-  if (!req.body.url) {
-    res.status(400).json({ error: 'No URL specified' });
-    return;
-  }
-
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-  res.header('Access-Control-Allow-Credentials', true);
-  // res.json({ message: 'Hello World', success: true });
-
-  var url = req.body.url;
-  // console.log('Requesting:', url);
-  try {
-    const resp = await fetch(url);
-    resp.text().then((text) => {
-      // console.log('Response:', text);
-      res.end(text);
-    });
-  } catch (err) {
-    console.error('Error proxying request:', err);
-    res
-      .status(500)
-      .json({ error: 'An error occurred while proxying the request' });
-  }
+app.post('/**', async (req, res) => {
+  getHandler(req, res);
 });
 
 var PORT = process.env.PORT || 8989;
