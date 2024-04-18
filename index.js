3a827deaf4c72afdc69bc6ac92a91fbf787a3182
denis.grimaliuc <denis.grimaliuc@crunchyroll.com>
2024-04-18 19:23:46 +0300

    test

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 const corsRoutes = require('./routes/enable-cors');
 const testRoutes = require('./routes/server');
 
